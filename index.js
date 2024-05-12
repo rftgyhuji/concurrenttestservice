c4d0f98169fafe29cad1f4ad2f04148e008436cc
rftgyhuji <gs1houwayek@hotmail.com>
2024-05-12 13:59:32 +0700

    First commit

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-function detectCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) {
-      let p1 = head;
-      let p2 = slow;
-      while (p1 !== p2) {
-        p1 = p1.next;
-        p2 = p2.next;
-      }
-      return p1;
-    }
-  }
-  return null;
-}
+const word = "hello";
+const reversedWord = word.split("").reverse().join("");
+console.log(reversedWord); // olleh
