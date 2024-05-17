c846ba7fa528bdf79421d4b195f67eebb907fa2a
rftgyhuji <gs1houwayek@hotmail.com>
2024-05-17 09:24:17 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-function longestCommonSubsequence(text1, text2) {
-  const m = text1.length;
-  const n = text2.length;
-  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (text1[i - 1] === text2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
-      else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+function isMatch(s, p) {
+  const dp = Array.from(Array(s.length + 1), () =>
+    Array(p.length + 1).fill(false),
+  );
+  dp[0][0] = true;
+  for (let i = 1; i <= p.length; i++) {
+    if (p[i - 1] === "*") {
+      dp[0][i] = dp[0][i - 2];
     }
   }
-  return dp[m][n];
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 1; j <= p.length; j++) {
+      if (s[i - 1] === p[j - 1] || p[j - 1] === ".") {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (p[j - 1] === "*") {
+        dp[i][j] = dp[i][j - 2];
+        if (p[j - 2] === "." || s[i - 1] === p[j - 2]) {
+          dp[i][j] = dp[i][j] || dp[i - 1][j];
+        }
+      }
+    }
+  }
+  return dp[s.length][p.length];
 }
